9a0f6dc1163535b4a62e93fbe5ea2d2bb149e536
roksana-barna <roksanabarna1803@gmail.com>
2023-05-01 17:52:14 +0600

    deploy

--- src/Shared/LeftNav/LeftNav.jsx
+++ src/Shared/LeftNav/LeftNav.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const LeftNav = () => {
  const [categories,setCategories]=useState([]);
  useEffect(()=>{
-    fetch('http://localhost:5000/categories')
+    fetch('https://the-dragon-server-roksana-barna.vercel.app/categories')
     .then(res =>res.json())
     .then(data=>setCategories(data))
     .catch(error =>console.error(error))
